a5e89be674820cd88380a30505885040007abae9
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2024-07-03 10:50:04 +0200

    Initial commit

---
+++ src/test/java/com/example/forumspringbootapp/ForumSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forumspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
